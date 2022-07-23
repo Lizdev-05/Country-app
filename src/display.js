8f10c2c58605396f1010ed86974deda44d06ef01
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-07-23 18:03:36 +0100

    Display countries wih Api

--- src/display.js
+++ src/display.js
@@ -1,1 +1,30 @@
-const API_URL = 'https://restcountries.com/v3.1/all';
+const API_URL = 'https://restcountries.com/v2/all';
+const cards = document.querySelector('.cards');
+
+const showCountry = (data) => {
+  const cardContainer = document.createElement('div');
+  cardContainer.classList.add('card');
+  cardContainer.innerHTML = `
+  <div class="card-image">
+  <img src="${data.flag}" />
+  </div>
+  <div class="card-text">
+  <h3>${data.name}</h3>
+  <p>Population: ${data.population / 1000000}</p>
+  <p>Region: ${data.region}</p>
+  <p>Capital city: ${data.capital}</p>
+  </div>
+    `;
+  cards.appendChild(cardContainer);
+};
+
+const display = async () => {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  data.forEach((country) => {
+    showCountry(country);
+  });
+};
+
+display();
+export default display;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './style.css';
 import './display.js';
+
+// const display = async (country) => {
+//   const response = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+//   const data = await response.json();
+//   console.log(data);
+// };
+// display('Germany');
